1419471cb0992e0647d55cd1d1e730dd9acc5459
nandanmathure@gmail.com <nandanmathure@gmail.com>
2020-05-10 00:42:32 -0400

    Fixed compilation issue

--- Src/NewtonsoftExtensions.Tests/TypeFamilyConverterTests.cs
+++ Src/NewtonsoftExtensions.Tests/TypeFamilyConverterTests.cs
@@ -8,46 +8,46 @@ using System.Linq;
 namespace CloudNDevOps.Newtonsoft.Extensions.Tests
 {
     [TestClass]
-    public TypeFamilyConverterTests
+    public class TypeFamilyConverterTests
     {
         [TestMethod]
-    public void TestMethod1()
-    {
-        // Arrange 
-        string inputString = "[ { \"Code\" : \"C1\", \"Classifier\" : \"Individual\", \"FirstName\" : \"John\", \"LastName\" : \"Doe\" }, " +
-            "{ \"Code\" : \"C2\", \"Classifier\" : \"Organization\", \"Name\" : \"Some Organization\" } ]";
-        var TypeFamilyConverter = new TypeFamilyConverter<Entity<string>, string>(
-            new Func<Entity<string>, string>(e => e.Classifier),
-            new Dictionary<string, Type>
-            {
+        public void TestMethod1()
+        {
+            // Arrange 
+            string inputString = "[ { \"Code\" : \"C1\", \"Classifier\" : \"Individual\", \"FirstName\" : \"John\", \"LastName\" : \"Doe\" }, " +
+                "{ \"Code\" : \"C2\", \"Classifier\" : \"Organization\", \"Name\" : \"Some Organization\" } ]";
+            var typeFamilyConverter = new TypeFamilyConverter<Entity<string>, string>(
+                new Func<Entity<string>, string>(e => e.Classifier),
+                new Dictionary<string, Type>
+                {
                     { "Individual", typeof(IndividualEntity<string>) },
                     { "Organization", typeof(OrganizationEntity<string>) }
-            });
+                });
 
-        // Act
-        var result = JsonConvert.DeserializeObject<List<Entity<string>>>(
-            inputString,
-            new JsonSerializerSettings
-            {
-                Converters = { tfc }
-            });
+            // Act
+            var result = JsonConvert.DeserializeObject<List<Entity<string>>>(
+                inputString,
+                new JsonSerializerSettings
+                {
+                    Converters = { typeFamilyConverter }
+                });
 
-        // Assert
-        result.Should().HaveCount(2);
+            // Assert
+            result.Should().HaveCount(2);
 
-        var fi = result.First(c => c.Code == "C1");
-        fi.Should().BeAssignableTo<IndividualEntity<string>>();
-        fi.Classifier.Should().Be("Individual");
-        var fiIndividual = (IndividualEntity<string>)fi;
-        fiIndividual.FirstName.Should().Be("John");
-        fiIndividual.LastName.Should().Be("Doe");
+            var fi = result.First(c => c.Code == "C1");
+            fi.Should().BeAssignableTo<IndividualEntity<string>>();
+            fi.Classifier.Should().Be("Individual");
+            var fiIndividual = (IndividualEntity<string>)fi;
+            fiIndividual.FirstName.Should().Be("John");
+            fiIndividual.LastName.Should().Be("Doe");
 
-        var si = result.First(c => c.Code == "C2");
-        si.Should().BeAssignableTo<OrganizationEntity<string>>();
-        si.Classifier.Should().Be("Organization");
-        var siOrganization = (OrganizationEntity<string>)si;
-        siOrganization.Name.Should().Be("Some Organization");
+            var si = result.First(c => c.Code == "C2");
+            si.Should().BeAssignableTo<OrganizationEntity<string>>();
+            si.Classifier.Should().Be("Organization");
+            var siOrganization = (OrganizationEntity<string>)si;
+            siOrganization.Name.Should().Be("Some Organization");
 
+        }
     }
-}
 }
